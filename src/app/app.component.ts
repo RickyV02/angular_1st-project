b48f6ec604c1802f6d4444c03ad46ab607023dab
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-12-01 21:22:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserName!: string;
 
+  onSelectUser(id: string) {
+    const selectedUser = this.users.find((user) => user.id === id);
+    this.selectedUserName = selectedUser ? selectedUser.name : '';
+  }
 }
